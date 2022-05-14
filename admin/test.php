61eeabcfec0154f3f1edb32564fc3756fbc4e06c
윤희동 <jini1237@naver.com>
2022-05-14 18:00:47 +0900

    ㅁㅁㅁ
    ㅁㅁㅁ

--- admin/test.php
+++ admin/test.php
@@ -1,11 +1,21 @@
 <?php
 
-echo "MySql 연결 테스트<br>";
- 
-//$con = mysqli_connect('3.36.219.35', 'root', 'apmsetup', 'met_prototype');
+/*
+$real_sock = mysqli_connect($real_hostname, $real_username, $real_password, $real_name, '3306');
+$real_db = mysqli_select_db($real_sock, $real_name) or die ("데이터베이스 서버에 연결할 수 없습니다.");
+mysqli_set_charset($real_sock, 'utf8'); 
 
-$connection = ssh2_connect('https://3.36.219.35', 22); 
-ssh2_auth_password($connection, 'root', 'apmsetup');
-$tunnel = ssh2_tunnel($connection, '127.0.0.1', 3306);
-$db = new mysqli_connect('127.0.0.1', 'root', 'apmsetup', 'met_prototype', 3306, $tunnel)    or die ('Fail: ' . mysql_error());  
+
+
+
+$connection = ssh2_connect('3.36.219.35', 22); 
+
+ssh2_auth_password($connection, 'ubuntu', '');
+$tunnel = ssh2_tunnel($connection, '127.0.0.1', 3307);
+$db = new mysqli_connect('127.0.0.1', 'root', 'autoset', 
+                         'autoset', 3307, $tunnel)
+    or die ('Fail: ' . mysql_error());  
+*/
+shell_exec("ssh -fNg -L 3307:$real_hostname:3306 user@remote_host");
+$conection = new mysqli($real_hostname, $real_username, $real_password, $real_name, 3307);
 ?>
