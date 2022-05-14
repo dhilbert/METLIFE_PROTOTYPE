d38ec4629cf115508b2916364051ad3333a0c0ef
윤희동 <jini1237@naver.com>
2022-05-14 18:03:06 +0900

    ㅁㅁㅁ
    ㅁㅁㅁㅁ

--- admin/test.php
+++ admin/test.php
@@ -18,4 +18,31 @@ $db = new mysqli_connect('127.0.0.1', 'root', 'autoset',
 */
 shell_exec("ssh -fNg -L 3307:$real_hostname:3306 user@remote_host");
 $conection = new mysqli($real_hostname, $real_username, $real_password, $real_name, 3307);
+
+
+
+
+
+
+
+
+
+
+
+
+echo "MySql 연결 테스트<br>";
+ 
+$con = mysqli_connect('3.36.219.35', 'root', 'apmsetup', 'met_prototype');
+ 
+if($con){
+    echo "connect : 성공<br>";
+}
+else{
+    echo "disconnect : 실패<br>";
+}
+ 
+$sql = 'SELECT VERSION()';
+$result = mysqli_query($con, $sql);
+$data = mysqli_fetch_assoc($result);
+echo $data['VERSION()'];
 ?>
