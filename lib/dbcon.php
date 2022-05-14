6d96ae0a09ee10130d8dad2136700c10b917609e
윤희동 <jini1237@naver.com>
2022-05-14 22:57:04 +0900

    1
    1

--- lib/dbcon.php
+++ lib/dbcon.php
@@ -8,10 +8,18 @@ $real_password="autoset";
 
 $real_name="met_prototype";
 
-$real_sock = mysql_connect($real_hostname, $real_username, $real_password, $real_name);
+$real_sock = mysqli_connect($real_hostname, $real_username, $real_password, $real_name);
+
+if ( $real_sock == false ) {
+    echo "<p>Failure - " . mysqli_connect_error() . "</p>";
+  } else {
+    echo "<p>Success</p>";
+  }
+
+
+
 $real_db = mysqli_select_db($real_sock, $real_name) or die ("데이터베이스 서버에 연결할 수 없습니다.");
 mysqli_set_charset($real_sock, 'utf8'); 
-
 
 
 
