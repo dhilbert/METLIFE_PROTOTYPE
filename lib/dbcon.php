eab69f1b6485048c65736a64fb0447dc613e06b1
윤희동 <jini1237@naver.com>
2022-05-22 21:57:11 +0900

    111
    111111

--- lib/dbcon.php
+++ lib/dbcon.php
@@ -2,9 +2,9 @@
 
 error_reporting( E_ALL );
   ini_set( "display_errors", 1 );
+include_once('function.php');
 
-
-  $real_hostname="3.36.83.6";
+$real_hostname="3.36.83.6";
 $real_username="cfgaussroot";
 //$real_username="gaussadmin";
 $real_password="autoset";
@@ -25,7 +25,7 @@ $real_db = mysqli_select_db($real_sock, $real_name) or die ("데이터베이스 
 mysqli_set_charset($real_sock, 'utf8'); 
 
 
-include_once('function.php');
+
 
 
 ?>
