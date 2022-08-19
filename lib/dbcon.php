48d6f187c1984f0b6e5d2a9efe07f02441934ec1
윤희동 <jini1237@naver.com>
2022-08-19 09:39:06 +0900

    DB Con cencel
    aaaa

--- lib/dbcon.php
+++ lib/dbcon.php
@@ -3,18 +3,18 @@
 error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
 include('function.php');
-
+/*
 $real_hostname="3.36.83.6";
 $real_username="cfgaussroot";
 //$real_username="gaussadmin";
 $real_password="autoset";
 
-/*
+
 $real_hostname="localhost";
 $real_username="root";
 $real_password="autoset";
 
-*/
+
 
 
 
@@ -23,7 +23,7 @@ $real_name="met_prototype";
 $real_sock = mysqli_connect($real_hostname, $real_username, $real_password, $real_name);
 $real_db = mysqli_select_db($real_sock, $real_name) or die ("데이터베이스 서버에 연결할 수 없습니다.");
 mysqli_set_charset($real_sock, 'utf8'); 
-
+*/
 function replace_texts($text){
 	$text = str_replace('"' , '\"', $text);
 	$text = str_replace("'" , "\'", $text);	
